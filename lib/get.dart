571ba168a81cb02b6f50a5f4cbe3cf841602c62a
baheej007 <baheejtpm@gmail.com>
2025-10-19 17:29:15 +0530

    animation

---
+++ lib/get.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatefulWidget {
+  const SecondPage({super.key});
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 100, end: 200).animate(_controller);
+
+    _controller.repeat(reverse: true); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Animated Box"),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Container(
+              width: _animation.value,
+              height: _animation.value,
+              color: Colors.blue,
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,19 +12,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Custom Paint"),
-          backgroundColor: Colors.amberAccent,
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.black,
-            child: CustomPaint(
-              painter: MasterPainter(),
-              size: const Size(400, 500),
+      home: const FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Custom Paint"),
+        backgroundColor: Colors.amberAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.black,
+                child: CustomPaint(
+                  painter: MasterPainter(),
+                  size: const Size(400, 500),
+                ),
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
+            IconButton(
+              onPressed: () {
+                
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SecondPage(),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.play_arrow_outlined,
+                size: 40,
+                color: Colors.amberAccent,
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -41,15 +75,10 @@ class MasterPainter extends CustomPainter {
     canvas.drawRect(Rect.fromLTWH(50, 50, 200, 150), paint);
 
     paint.color = Colors.green;
-    canvas.drawCircle(Offset(200, 300), 50, paint);
+    canvas.drawCircle(const Offset(200, 300), 50, paint);
 
-    paint.color = Colors.blue;
-    canvas.drawLine(Offset(100, 400), Offset(300, 450), paint);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
-
-
-
