745eb873c2d09299e4322102910f338fa120db98
baheej007 <baheejtpm@gmail.com>
2025-11-11 12:11:03 +0530

    card

--- lib/main.dart
+++ lib/main.dart
@@ -23,67 +23,79 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Image(
-    image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSHxhkjrPiUJPHjsB0kuD5pOSejARhOwDtzA&s'),
-    fit: BoxFit.cover,
-  ),
-        
-  title: const Text("App Bar"),
-  elevation: 5,
-  shadowColor: const Color.fromARGB(255, 25, 255, 4),
-  backgroundColor: Colors.amberAccent,
-  shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.horizontal(
-        left: Radius.circular(18),
-        right: Radius.circular(18),
+    return DefaultTabController(
+      length: 3, 
+
+      child: Scaffold(
+        appBar: AppBar(
+          
+          flexibleSpace: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0kr5b5sxYqLRQuDCVEoJM4yJhAsNSWK2xVg&s'),
+            fit: BoxFit.cover,
+          ),
+          title: const Text("App Bar"),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(18),
+              right: Radius.circular(18),
+            ),
+          ),
+
+          bottom:  TabBar(
+            padding: EdgeInsets.symmetric(vertical: 4,),
+            tabs: [
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(width: double.infinity,
+                          padding: EdgeInsets.symmetric(vertical: 3, horizontal: 12),
+                          color: const Color.fromARGB(255, 172, 15, 4),
+                          child: Text("Home",style:TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
+                        ),
+                ),
+              ),
+      Expanded(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 3, horizontal: 12),
+            color: const Color.fromARGB(255, 159, 170, 2),
+            child: Text("Chat",style:TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
+          ),
+        ),
       ),
-    ),
-    actions: [
-      IconButton(
-        icon: Icon(Icons.notifications),
-        onPressed: () {
-          Navigator.push(
+      Expanded(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 3, horizontal: 10),
+            color: const Color.fromARGB(255, 3, 123, 7),
+            child: Text("Profiles",style:TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),),
+          ),
+        ),
+      ),
+            ],
+          ), 
+
+          actions: [
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {
+                Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => const ThirdPage(),
                   ),
                 );
-        },
-      ),
-      IconButton(
-        icon: Icon(Icons.settings),
-        onPressed: () {
-          Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SecondPage(),
-                  ),
-                );
-        },
-      ),
-    ],
-  ),
-
-      
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.black,
-                child: CustomPaint(
-                  painter: MasterPainter(),
-                  size: const Size(400, 500),
-                ),
-              ),
+              },
             ),
-            const SizedBox(height: 20),
             IconButton(
+              icon: Icon(Icons.settings),
               onPressed: () {
-                
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -91,12 +103,16 @@ class FirstPage extends StatelessWidget {
                   ),
                 );
               },
-              icon: const Icon(
-                Icons.play_arrow_outlined,
-                size: 40,
-                color: Colors.amberAccent,
-              ),
             ),
+          ],
+        ),
+
+       
+        body: TabBarView(
+          children: [
+            homeWidget(context),  
+            chatWidget(context),  
+            profileWidget(context) 
           ],
         ),
       ),
@@ -104,22 +120,86 @@ class FirstPage extends StatelessWidget {
   }
 }
 
-class MasterPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 5
-      ..style = PaintingStyle.stroke;
+Widget homeWidget(BuildContext context) {
+  return Center(
+    child: Container(
+      color: const Color.fromARGB(255, 121, 0, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Center(child: Text("Home"))),
+          const SizedBox(height: 20),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondPage()),
+              );
+            },
+            icon: const Icon(
+              Icons.play_arrow_outlined,
+              size: 40,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
 
-    canvas.drawRect(Rect.fromLTWH(50, 50, 200, 150), paint);
-
-    paint.color = Colors.green;
-    canvas.drawCircle(const Offset(200, 300), 50, paint);
-
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+Widget chatWidget(BuildContext context) {
+  return Center(
+    child: Container(
+      color: const Color.fromARGB(255, 199, 180, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Center(child: Text("chat"))),
+          const SizedBox(height: 20),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondPage()),
+              );
+            },
+            icon: const Icon(
+              Icons.play_arrow_outlined,
+              size: 40,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+Widget profileWidget(BuildContext context) {
+  return Center(
+    child: Container(
+      color: const Color.fromARGB(255, 5, 95, 11),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Center(child: Text("profile"))),
+          const SizedBox(height: 20),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondPage()),
+              );
+            },
+            icon: const Icon(
+              Icons.play_arrow_outlined,
+              size: 40,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
 
